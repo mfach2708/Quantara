71adbc464b735e670ccf7867cfe739402c9f5373
mfach2708 <fakhriahmadf99@gmail.com>
2026-03-26 13:25:03 +0700

    railway

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
